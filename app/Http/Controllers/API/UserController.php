4b012664e4e3f7e672a685e113ecf3798aef43b5
Dmitry <incapsulate@gmail.com>
2020-06-16 10:36:17 +0300

    feature/rest-api: add user-profile endpoint

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -12,7 +13,7 @@ class UserController extends Controller
     {
         return response()->json([
             'status' => 'success',
-            'data'   => $user->toJson(),
+            'data'   => new UserResource($user),
         ]);
     }
 }
